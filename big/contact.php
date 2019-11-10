53b3d08bb7cf00a6128917eda7afafbf28d1e659
norapeach <veganmuffcakes@gmail.com>
2019-11-10 12:04:37 -0800

    Adds BIG directory including draft version of BIG website files

---
+++ big/contact.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+ <head>
+     <title>Web Developer Examples and Resources by Nora Peachey</title>
+     <meta charset="utf-8" />
+     <meta name="robots" content="noindex,nofollow" />
+     <meta name="viewport" content="width=device-width" />
+     <link rel="stylesheet" href="css/big.css" />
+     <link rel="stylesheet" href="css/nav.css" />
+     <!-- jQuery -->
+     <script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
+     <!-- MenuMaker Plugin -->
+     <script src="https://s3.amazonaws.com/menumaker/menumaker.min.js" type="text/javascript"></script>
+     
+     <!-- Icon Library -->
+     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css" />
+     <!-- JS -->
+     <script src="js/script.js"></script>
+ </head>
+ <body>
+     <main>
+         <header>
+             <h1><a href="index.html">Web Developer Examples and Resources by Nora</a></h1>
+             <nav id="cssmenu">
+                  <ul>
+                     <li><a href="../index.html"><i class="fa fa-fw fa-institution" aria-hidden="true"></i> WEB110</a></li>
+                     <li><a href="index.html"><i class="fa fa-fw fa-home" aria-hidden="true"></i> BIG</a></li>
+                     <!-- Drop down for research pages -->
+                     <li><a href="#">Research</a>
+                        <ul>
+                           <li><a href="accessibility.html"><i class="fa fa-fw fa-globe" aria-hidden="true"></i> Accessibility</a></li>
+                           <li><a href="smo.html"><i class="fa fa-fw fa-thumbs-o-up" aria-hidden="true"></i> SMO</a></li>
+                           <li><a href="forms.html"><i class="fa fa-check-square-o" aria-hidden="true"></i> HTML Forms</a></li>
+                        </ul>
+                     </li>
+                     <li><a href="gallery.html"><i class="fa fa-fw fa-camera-retro" aria-hidden="true"></i> Lightbox II Gallery</a></li>
+                     <!-- Drop down for Google Tools -->
+                     <li><a href="#">Google Tools</a>
+                        <ul>
+                           <li><a href="seo.html"><i class="fa fa-fw fa-desktop" aria-hidden="true"></i> SEO</a></li>
+                           <li><a href="calendar.html"><i class="fa fa-fw fa-calendar" aria-hidden="true"></i> Calendar</a></li>
+                           <li><a href="map.html"><i class="fa fa-fw fa-map-o" aria-hidden="true"></i> Map</a></li>
+                        </ul>
+                     </li>
+                     <li><a href="contact.php"><i class="fa fa-fw fa-server" aria-hidden="true"></i> Contact</a></li>
+                  </ul>
+             </nav>
+        </header>
+        <!-- START LEFT COLUMN -->
+        <section class="fullwidth">
+            <h2 class="subheader">Contact Nora</h2>
+            <p>This will be updated with multi-field form.</p>
+        </section>
+        <!-- END LEFT COLUMN -->
+
+        <footer>
+          <p><small>&copy; 2019 by <a href="contact.php">Contact Nora Peachey </a>, All Rights Reserved ~ <a href="http://validator.w3.org/check/referer" target="_blank">Valid HTML</a> ~ <a href="http://jigsaw.w3.org/css-validator/check?uri=referer" target="_blank">Valid CSS</a></small></p>
+        </footer>
+    </main>
+     
+     
+ </body>
+</html>
---
+++ big/js/script.js
@@ -0,0 +1,11 @@
+(function($){
+$(document).ready(function(){
+
+$("#cssmenu").menumaker({
+    title: "Menu",
+    breakpoint: 1018,
+    format: "multitoggle"
+});
+
+});
+})(jQuery);
